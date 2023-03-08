d449ea15d87fa9868c079860fe59052149c734d6
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-08 22:41:07 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp4.1.17/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp4._1._17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на ангийском с маленькой буквы");
+            var color = Console.ReadLine();
+
+            if (color == "red") 
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+            else if (color == "green") 
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else 
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
+        }
+    }
+}
