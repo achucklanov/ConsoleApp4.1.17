27030db0b43fb609dfdb2a65514f9c93de3d70f3
AndreyChuck <a.chucklanov@yandex.ru>
2023-11-24 21:28:37 +0400

    изменения в строках 7, 10, 17.

--- ConsoleApp4.1.17/Program.cs
+++ ConsoleApp4.1.17/Program.cs
@@ -4,17 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напишите свой любимый цвет на ангийском языке с маленькой буквы:");
+            Console.WriteLine("Напишите свой любимый цвет на русском или ангийском языке с маленькой буквы:");
             var color = Console.ReadLine();
 
-            if (color == "red") 
+            if (color == "красный" | color == "red")
             {
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.ForegroundColor = ConsoleColor.Black;
 
                 Console.WriteLine("Your color is red!");
             }
-            else if (color == "green") 
+            else if (color == "зелёный" | color == "green") 
             {
                 Console.BackgroundColor = ConsoleColor.Green;
                 Console.ForegroundColor = ConsoleColor.Black;
